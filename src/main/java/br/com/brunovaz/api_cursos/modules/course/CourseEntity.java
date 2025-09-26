6569d171d599a0d011e1b0cb88b4a8d68016156e
Bruno Abreu <vazs.bruno@gmail.com>
2025-09-26 20:05:54 -0300

    feat: Criar tabela do curso no banco

---
+++ src/main/java/br/com/brunovaz/api_cursos/modules/course/CourseEntity.java
@@ -0,0 +1,40 @@
+package br.com.brunovaz.api_cursos.modules.course;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.ValueGenerationType;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name="course")
+public class CourseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String name;
+    private String category;
+
+    @Enumerated(EnumType.STRING)
+    private EnumCourseActive active;
+
+    @CreationTimestamp
+    private LocalDateTime created_at;
+    
+    @UpdateTimestamp
+    private LocalDateTime updated_at;
+    
+    public enum EnumCourseActive {
+        ACTIVE,
+        DISABLED
+    }
+}
